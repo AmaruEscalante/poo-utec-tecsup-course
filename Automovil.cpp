e74434863287d3a50f2880630a0e58b16e50f899
AmaruEscalante <sebastian.escalante@utec.edu.pe>
2019-09-29 21:07:21 -0500

    added atributes and functions to the class Automovil

--- Automovil.cpp
+++ Automovil.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "Automovil.h"
+
+void Automovil::turn_on() {
+
+}
+
+void Automovil::accelerate() {
+
+}
+
+void Automovil::stop() {
+
+}
+
+void Automovil::brake() {
+
+}
+
+void Automovil::direction(int valor) {
+
+}
+
+std::string Automovil::get_owner() {
+    return std::__cxx11::string();
+}
+
+std::string Automovil::get_model() {
+    return std::__cxx11::string();
+}
+
+int Automovil::get_velocity() {
+    return 0;
+}
--- Automovil.h
+++ Automovil.h
@@ -4,10 +4,29 @@
 
 #ifndef GAME_NEW_AUTOMOVIL_H
 #define GAME_NEW_AUTOMOVIL_H
-
+#include <string>
 
 class Automovil {
-
+private:
+    int x;
+    int y;
+    int height;
+    int width;
+    char color;
+    std::string model;
+    std::string owner;
+    int vx;
+    int vy;
+    int direction;
+public:
+    void turn_on();
+    void accelerate();
+    void stop();
+    void brake();
+    void direction_f(int valor);
+    std::string get_owner();
+    std::string get_model();
+    int get_velocity();
 };
 
 
